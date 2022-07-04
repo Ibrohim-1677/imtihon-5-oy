110b5d918baa9102b072864092f65f8fe831a2b9
Ibrohim-1677 <Ibrohimtalatov58@gmail.com>
2022-07-04 23:21:09 +0300

    initial commit

---
+++ src/appa.js
@@ -0,0 +1,16 @@
+
+// import { BrowserRouter, Route,Routes } from "react-router-dom";
+// import FeedbackId from "./module/private/feedback/feedbackId";
+
+
+// function Roat() {
+//   return (
+//     <>
+//     <Routes>
+//       <Route path="/feedback/:id" element={<FeedbackId />} />
+//     </Routes>
+//   </>
+//   );
+// }
+
+// export default Roat;
